2093cde1f659a3a5a057ba38da136fbc3ab632c8
nkartha <nalinikartha@gmail.com>
2014-08-18 01:11:29 -0700

    Rename data file to trainingData, add test data file, linear model for price prediction.

--- predictPrice.R
+++ predictPrice.R
@@ -13,10 +13,14 @@ CleanData <- function(housingData) {
   
   # Asking and Sales Prices need to be cleaned (commas and dollar signs stripped) and converted to numerics
   housingData$AskingPrice <- as.numeric(gsub(",","", (gsub("\\$","", housingData$AskingPrice))))
-  housingData$SalePrice <- as.numeric(gsub(",","", (gsub("\\$","", housingData$SalePrice))))
+  if("SalePrice" %in% colnames(housingData))
+  {
+    housingData$SalePrice <- as.numeric(gsub(",","", (gsub("\\$","", housingData$SalePrice))))
+  }
   
-  # Closing date should be a date
-  housingData$ClosingDate <- as.Date(housingData$ClosingDate, "%m/%d/%y")
+  # Closing date can be converted to number of days since the beginning of time, let's use earliest closing date.
+  beginningOfTime <- as.Date("02/28/07", "%m/%d/%y")summary()
+  housingData$ClosingDate <- as.numeric(as.Date(housingData$ClosingDate, "%m/%d/%y") - beginningOfTime)
   
   # Days on market needs to be cleaned ("N/A" values set to 0, need to think about this..) and converted to numerics
   housingData$DaysOnMarket <- as.numeric(gsub("N/A","0", housingData$DaysOnMarket))
@@ -24,10 +28,12 @@ CleanData <- function(housingData) {
   return(housingData)
 }
 
-housingData <- read.table(file="peterCouttsData.txt", header = TRUE)
+housingData <- read.table(file="peterCouttsTrainingData.txt", header = TRUE)
 cleanedData <- CleanData(housingData);
-features <- subset(cleanedData, select = c(UnitType, BR, BA, SQFT, AskingPrice, ClosingDate, DaysOnMarket))
-labels <- subset(cleanedData, select = c(SalePrice))
 
-  
+pricePredictionLm <- lm(SalePrice ~ UnitType + BR + BA + SQFT + AskingPrice + ClosingDate + DaysOnMarket, cleanedData)
+# Test data
+testData <- read.table(file="peterCouttsTestData.txt", header = TRUE)
+cleanedTestData <- CleanData(testData)
 
+predictedPrice <- predict(pricePredictionLm, cleanedTestData)
